31ed0ae50c8fc47a427c72b0bf658cf2a07a19a6
ysyx_22050533-武泽伟 <17805481057@163.com>
2023-01-12 10:02:23 +0800

    1月12号

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -31,38 +31,38 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   /*};*/
 }
 
-static void *memcpy(void *out, const void *in, size_t n) {
-	char *csrc=(char*)in;
-	char *cdest=(char*)out;
-	for(int i=0;i<n;i++)
-		cdest[i]=csrc[i];
-	return cdest;
-	panic("Not implemented");
-}
+/*static void *memcpy(void *out, const void *in, size_t n) {*/
+	/*char *csrc=(char*)in;*/
+	/*char *cdest=(char*)out;*/
+	/*for(int i=0;i<n;i++)*/
+		/*cdest[i]=csrc[i];*/
+	/*return cdest;*/
+	/*panic("Not implemented");*/
+/*}*/
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 		/*int wl = io_read(AM_GPU_CONFIG).width ;*/
-	  int w=ctl->w;
-		int h=ctl->h;
+		/*int w=ctl->w;*/
+		/*int h=ctl->h;*/
 		int wl = io_read(AM_GPU_CONFIG).width ;
-		int hl = io_read(AM_GPU_CONFIG).height ;
-		int min;
-		int x=ctl->x;
-		int y=ctl->y;
-		if(wl-x<w)
-			min=wl-x;
-		else 
-			min=w;
-		int size_pixels_copy=sizeof(uint32_t)*min;
+		/*int hl = io_read(AM_GPU_CONFIG).height ;*/
+		/*int min;*/
+		/*int x=ctl->x;*/
+		/*int y=ctl->y;*/
+		/*if(wl-x<w)*/
+			/*min=wl-x;*/
+		/*else */
+			/*min=w;*/
+		/*int size_pixels_copy=sizeof(uint32_t)*min;*/
 		uint32_t *color_buf=(uint32_t *)ctl->pixels;
 		uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-		/*for(int h=0;h<ctl->h;h++){*/
-			/*for(int w=0;w<ctl->w;w++){*/
-				/*fb[(ctl->y+h)*wl+w+ctl->x]=color_buf[w*h];*/
-			/*}}*/
-		for(int i = 0;i<h&&y+i<hl;i++){
-				memcpy(&fb[(y + i) * wl + x], color_buf, size_pixels_copy);
-				color_buf += w;
-			}
+		for(int h=0;h<ctl->h;h++){
+			for(int w=0;w<ctl->w;w++){
+				fb[(ctl->y+h)*wl+w+ctl->x]=color_buf[h*(ctl->w)+w];
+			}}
+		/*for(int i = 0;i<h&&y+i<hl;i++){*/
+				/*memcpy(&fb[(y + i) * wl + x], color_buf, size_pixels_copy);*/
+				/*color_buf += w;*/
+			/*}*/
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
