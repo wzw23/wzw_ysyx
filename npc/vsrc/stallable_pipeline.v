f373a2f5d91910b076f67f801c098dd5d9143210
ysyx_22050533-武泽伟 <17805481057@163.com>
2023-04-28 09:25:46 +0800

    未接入流水线前

---
+++ npc/vsrc/stallable_pipeline.v
@@ -0,0 +1,146 @@
+module stallable_pipeline(
+	input clk,
+	input rst,
+	input validin,
+	input [31:0]inst,
+	//id
+	input [11:0]e_j_b_inst,
+	output reg [31:0]inst_reg_id,
+	output reg [11:0]e_j_b_inst_reg_id,
+	//is
+	input [3:0]alu_src1,
+	input [2:0]alu_src2,
+	input [16:0]alu_control,
+	input data_ram_ren,
+	input data_ram_wen,
+	input [7:0]wmask,
+	input [2:0]sel_rf_res,
+	input [6:0]l_choose,
+	input w_choose,
+	input rf_wen,
+	input [63:0]src1,
+	input [63:0]src2,
+	input [63:0]rd,//rf waddr
+	input [63:0]imm,
+	input [63:0]c_rdata,
+	output reg [3:0]alu_src1_reg_is,
+	output reg [2:0]alu_src2_reg_is,
+	output reg [16:0]alu_control_reg_is,
+	output reg data_ram_ren_reg_is,
+	output reg data_ram_wen_reg_is,
+	output reg [7:0]wmask_reg_is,
+	output reg [2:0]sel_rf_res_reg_is,
+	output reg [6:0]l_choose_reg_is,
+	output reg w_choose_reg_is,
+	output reg rf_wen_reg_is,
+	output reg [63:0]src1_reg_is,
+	output reg [63:0]src2_reg_is,
+	output reg [63:0]rd_reg_is,//rf waddr
+	output reg [63:0]imm_reg_is,
+	output reg [63:0]c_rdata_reg_is,
+	output reg [11:0]e_j_b_inst_reg_is,
+	//wb
+	//input [2:0]sel_rf_res,
+	//input rf_wen,
+	input [63:0]alu_result,
+	input [63:0]ram_data,
+	//input [63:0]rd,
+	//input  [63:0]c_rdata,
+	output reg [2:0]sel_rf_res_reg_wb,
+	output reg rf_wen_reg_wb,
+	output reg [63:0]alu_result_reg_wb,
+	output reg [63:0]ram_data_reg_wb,
+	output reg [63:0]rd_reg_wb,
+	output reg [63:0]c_rdata_reg_wb,
+	
+	input out_allow,
+	output validout,
+
+	output id_reg_finish,
+  output is_reg_finish,
+	output wb_reg_finish
+
+);
+//pipe1 id
+//pipe2 ls
+//pipe3 wb
+	reg pipe1_valid;
+	reg pipe2_valid;
+	reg pipe3_valid;
+
+	wire pipe1_allow_in;
+	wire pipe1_ready_go;
+	wire pipe1_to_pipe2_valid;
+
+	wire pipe2_allow_in;
+	wire pipe2_ready_go;
+	wire pipe2_to_pipe3_valid;
+	
+	wire pipe3_allow_in;
+	wire pipe3_ready_go;
+
+//pipe1
+	assign pipe1_ready_go=1;//译码是瞬时完成的
+	assign pipe1_allow_in=!pipe1_valid||pipe1_ready_go&&pipe2_allow_in;//若!pipe1_valid&&pipe2_allowin表示pipe1向pipe2传输的是无效数据 并且已经传输 若pipe1_valid&&pipe1_ready_go&&pipe2_allowin表示传输的是有效数据 并且将在下个周期传输
+	assign pipe1_to_pipe2_valid=pipe1_valid&&pipe1_ready_go;//认为教科书上解释错误 此处信号表示的原因是pipe1传输给pipe2的信号是否有效 即总线上的valid信号
+	assign id_reg_finish=validin&&pipe1_allow_in;
+	always@(posedge clk)begin
+		if(rst)
+			pipe1_valid<='d0;
+		else if(pipe1_allow_in)
+			pipe1_valid<=validin;
+		if(validin&&pipe1_allow_in)begin
+			inst_reg_id<=inst;
+			e_j_b_inst_reg_id<=e_j_b_inst;
+		end
+	end
+//pipe2
+ assign pipe2_ready_go=1;//mem_finish时表示exe（is）完成
+ 	assign pipe2_allow_in=!pipe2_valid||pipe2_ready_go&&pipe3_allow_in;
+	assign pipe2_to_pipe3_valid=pipe2_valid&&pipe2_ready_go;
+	assign is_reg_finish=pipe1_to_pipe2_valid&&pipe2_allow_in;
+	always@(posedge clk)begin
+		if(rst)
+			pipe2_valid<='d0;
+		else if(pipe2_allow_in)
+			pipe2_valid<=pipe1_to_pipe2_valid;
+		if(pipe1_to_pipe2_valid&&pipe2_allow_in)begin
+			alu_src1_reg_is<=alu_src1;
+			alu_src2_reg_is<=alu_src2;
+			alu_control_reg_is<=alu_control;
+			data_ram_ren_reg_is<=data_ram_ren;
+			data_ram_wen_reg_is<=data_ram_wen;
+			wmask_reg_is<=wmask;
+			sel_rf_res_reg_is<=sel_rf_res;
+			l_choose_reg_is<=l_choose;
+			w_choose_reg_is<=w_choose;
+			rf_wen_reg_is<=rf_wen;
+			src1_reg_is<=src1;
+			src2_reg_is<=src2;
+			rd_reg_is<=rd;
+			imm_reg_is<=imm;
+			c_rdata_reg_is<=c_rdata;
+			e_j_b_inst_reg_is<=e_j_b_inst_reg_id;
+		end
+	end
+//pipe3
+ assign pipe3_ready_go=1;//对于寄存器来说写回信号一定能在一个周期内完成 因此
+ //写回信号恒为1
+ 	assign pipe3_allow_in=!pipe3_valid||pipe3_ready_go&&out_allow;//out_allow表示是否能一直输出 此处设置为1
+	assign wb_reg_finish=pipe2_to_pipe3_valid&&pipe3_allow_in;
+	always@(posedge clk)begin
+		if(rst)
+			pipe3_valid<='d0;
+		else if(pipe3_allow_in)
+			pipe3_valid<=pipe2_to_pipe3_valid;
+		if(pipe2_to_pipe3_valid&&pipe3_allow_in)begin
+			sel_rf_res_reg_wb<=sel_rf_res_reg_is;
+			rf_wen_reg_wb<=rf_wen_reg_is;
+			alu_result_reg_wb<=alu_result;
+			ram_data_reg_wb<=ram_data;
+			rd_reg_wb<=rd_reg_is;
+			c_rdata_reg_wb<=c_rdata_reg_is;
+		end
+	end
+	assign validout=pipe3_valid&&pipe3_ready_go;//新的结束信号
+endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -20,90 +20,74 @@ module top(
 	wire [63:0]imm;
 	wire [63:0]src1;
 	wire [63:0]src2;
-  // 位宽为1比特, 复位值为1'b1, 写使能一直有效
-  //Reg #(1, 1'b1) i0 (clk, rst, in[0], out[0], 1'b1);
-  // 位宽为3比特, 复位值为3'b0, 写使能为out[0]
-  //Reg #(3, 3'b0) i1 (clk, rst, in[3:1], out[3:1], out[0]);
-  //pc test
-	
-	//IF模块
-	//wire [31:0]inst;
-	//wire [31:0]araddr_1;wire arvalid_1;wire arready_1;wire [63:0]rdata_1;wire [1:0]rresp_1;wire rvalid_1;wire rready_1;
-  //If i0(clk, rst,cpupc,sel_nextpc,imm,src1,inst,dnpc,c_rdata,inst_update,mem_finish,.araddr(araddr_1),.arvalid(arvalid_1),.arready(arready_1),.rdata(rdata_1),.rresp(rresp_1),.rvalid(rvalid_1),.rready(rready_1));//if
 
-wire [31:0]araddr1;
-wire arvalid1;
-wire [1:0]arburst1;
-wire [7:0]arlen1;
-wire [2:0]arsize1;
-wire arready1;
-wire [63:0]rdata1;
-wire [1:0]rresp1;
-wire rvalid1;
-wire rlast1;
-wire rready1;
+	wire [31:0]araddr1;
+	wire arvalid1;
+	wire [1:0]arburst1;
+	wire [7:0]arlen1;
+	wire [2:0]arsize1;
+	wire arready1;
+	wire [63:0]rdata1;
+	wire [1:0]rresp1;
+	wire rvalid1;
+	wire rlast1;
+	wire rready1;
 
-wire [31:0]araddr2;
-wire arvalid2;
-wire [1:0]arburst2;
-wire [7:0]arlen2;
-wire [2:0]arsize2;
-wire arready2;
-wire [63:0]rdata2;
-wire [1:0]rresp2;
-wire rvalid2;
-wire rlast2;
-wire rready2;
- 
-wire [31:0]awaddr2;
-wire awvalid2;
-wire [1:0]awburst2;
-wire [7:0]awlen2;
-wire awready2;
-wire [63:0]wdata2;
-wire wlast2;
-wire [7:0]wstrb2;
-wire wvalid2;
-wire wready2;
-wire [1:0]bresp2;
-wire bvalid2;
-wire bready2;
-wire [63:0]dnpc_jump_data;
+	wire [31:0]araddr2;
+	wire arvalid2;
+	wire [1:0]arburst2;
+	wire [7:0]arlen2;
+	wire [2:0]arsize2;
+	wire arready2;
+	wire [63:0]rdata2;
+	wire [1:0]rresp2;
+	wire rvalid2;
+	wire rlast2;
+	wire rready2;
 
-wire [11:0]e_j_b_inst;
- If if_1(
-.clk(clk), 
-.rst(rst),
-.cpupc(cpupc),
-.sel_nextpc(sel_nextpc),
-.imm(imm),
-.src1(src1),
-.inst(inst),
-.dnpc(dnpc),
-.c_rdata(c_rdata),
-.inst_update(inst_update),
-.mem_finish(mem_finish),
-.araddr1(araddr1),
-.arvalid1(arvalid1),
-.arburst1(arburst1),
-.arlen1(arlen1),
-.arsize1(arsize1),
-.arready1(arready1),
-.rdata1(rdata1),
-.rresp1(rresp1),
-.rvalid1(rvalid1),
-.rlast1(rlast1),
-.rready1(rready1),
-.e_j_b_inst(e_j_b_inst),
-.dnpc_jump_data(dnpc_jump_data)
-);//if
-  //decode_exec de(clk,inst,cpupc,dnpc);
+	wire [31:0]awaddr2;
+	wire awvalid2;
+	wire [1:0]awburst2;
+	wire [7:0]awlen2;
+	wire awready2;
+	wire [63:0]wdata2;
+	wire wlast2;
+	wire [7:0]wstrb2;
+	wire wvalid2;
+	wire wready2;
+	wire [1:0]bresp2;
+	wire bvalid2;
+	wire bready2;
+	wire [63:0]dnpc_jump_data;
 
-/*wire [11:0]e_j_b_inst;*/
-/*wire not_jump;*/
-/*pre_decode pre_decode_0(.clk(clk),.rst(rst),.inst(inst),.e_j_b_inst(e_j_b_inst),.not_jump(not_jump));*/
-///////////////////////////////////////////
-	
+	wire [11:0]e_j_b_inst;
+	If if_1(
+		.clk(clk), 
+		.rst(rst),
+		.cpupc(cpupc),
+		.sel_nextpc(sel_nextpc),
+		.imm(imm),
+		.src1(src1),
+		.inst(inst),
+		.dnpc(dnpc),
+		.c_rdata(c_rdata),
+		.inst_update(inst_update),
+		.mem_finish(mem_finish),
+		.araddr1(araddr1),
+		.arvalid1(arvalid1),
+		.arburst1(arburst1),
+		.arlen1(arlen1),
+		.arsize1(arsize1),
+		.arready1(arready1),
+		.rdata1(rdata1),
+		.rresp1(rresp1),
+		.rvalid1(rvalid1),
+		.rlast1(rlast1),
+		.rready1(rready1),
+		.e_j_b_inst(e_j_b_inst),
+		.dnpc_jump_data(dnpc_jump_data)
+	);//if
+/////////////////////id//////////////////////
 	wire [4:0]rs1;
 	wire [4:0]rs2;
 	wire [4:0]rd;
@@ -111,7 +95,6 @@ wire [11:0]e_j_b_inst;
 	wire [4:0]fu_7_d;
 	wire [7:0]fu_3_d;
 	wire [1:0]c_waddr;
-	//wire [2:0]e_inst;
 	id id_1(.clk(clk),.rst(rst),.inst(inst),.rs1(rs1),.rs2(rs2),.rd(rd),.imm(imm),.op_d(op_d),.fu_7_d(fu_7_d),.fu_3_d(fu_3_d),.e_j_b_inst(e_j_b_inst),.c_waddr(c_waddr),.rf_wen(rf_wen),.wdata(wdata),.src1(src1),.src2(src2),.c_rdata(c_rdata),.c_wchoose(c_wchoose),.c_wen(c_wen),.c_wen1_2(c_wen1_2),.cpupc(cpupc));
 	assign ebreak=e_j_b_inst[0];
 	wire [3:0]sel_alu_src1;
@@ -129,119 +112,102 @@ wire [11:0]e_j_b_inst;
 	wire [7:0]wmask;
 	wire c_wen1_2;
 	control control_1(.op_d(op_d),.fu_7_d(fu_7_d),.fu_3_d(fu_3_d),.sel_alu_src1(sel_alu_src1),.sel_alu_src2(sel_alu_src2),.alu_control(alu_control),.rf_wen(rf_wen),.sel_rf_res(sel_rf_res),.data_ram_en(data_ram_en),.data_ram_wen(data_ram_wen),.wmask(wmask),.l_choose(l_choose),.not_have(not_have),.w_choose(w_choose),.c_wchoose(c_wchoose),.c_wen(c_wen),.e_j_b_inst(e_j_b_inst),.c_wen1_2(c_wen1_2),.mem_finish(mem_finish)) ;//控制模块
-	/////////////////////////////////////////
+/////////////////////////////////////////
+///////////////////is//////////////////////
 
 	wire [63:0]alu_result;
 	wire [63:0]ram_addr;
 	wire [63:0]ram_data;
 	wire [63:0]wdata;
-	//exe exe_1(.clk(clk),.rst(rst),.imm(imm),.rs1(rs1),.rs2(rs2),.rd(rd),.sel_alu_src1(sel_alu_src1),.sel_alu_src2(sel_alu_src2),.alu_control(alu_control),.rf_wen(rf_wen),.wdata(wdata),.alu_result(alu_result),.ram_addr(ram_addr),.src1(src1),.alu_equal(alu_equal),.cpupc(cpupc),.w_choose(w_choose),.src2(src2),.c_wchoose(c_wchoose),.c_wen(c_wen),.c_raddr(c_raddr),.c_waddr(c_waddr),.c_rdata(c_rdata),.e_inst(e_inst),.c_wen1_2(c_wen1_2));
 	exe exe_1(.clk(clk),.rst(rst),.imm(imm),.sel_alu_src1(sel_alu_src1),.sel_alu_src2(sel_alu_src2),.alu_control(alu_control),.alu_result(alu_result),.ram_addr(ram_addr),.src1(src1),.alu_equal(alu_equal),.cpupc(cpupc),.w_choose(w_choose),.src2(src2),.e_j_b_inst(e_j_b_inst),.c_rdata(c_rdata),.dnpc_jump_data(dnpc_jump_data));
-
 	//访存模块
-	
- /* wire [31:0]araddr_2;*/
-	/*wire arvalid_2;*/
-	/*wire arready_2;*/
-	/*wire [63:0]rdata_2;*/
-	/*wire [1:0]rresp_2;*/
-	/*wire rvalid_2;*/
-	/*wire rready_2;*/
-	/*wire [31:0]awaddr_2;*/
-	/*wire awvalid_2;*/
-	/*wire awready_2;*/
-	/*wire [63:0]wdata_2;*/
-	/*wire [7:0]wstrb_2;*/
-	/*wire wvalid_2;*/
-	/*wire wready_2;*/
-	/*wire [1:0]bresp_2;*/
-	/*wire bvalid_2;*/
-	/*wire bready_2;*/
-	//mem #(64,64) mem0(.clk(clk),.rst(rst),.r_ren(data_ram_en),.r_raddr(ram_addr),.r_rdata(ram_data),.r_waddr(ram_addr),.r_mask(wmask),.r_wen(data_ram_wen),.r_wdata(src2),.l_choose(l_choose),.inst_update(inst_update),.mem_finish(mem_finish),.inst_finish(inst_finish),.araddr(araddr_2),.arvalid(arvalid_2),.arready(arready_2),.rdata(rdata_2),.rresp(rresp_2),.rvalid(rvalid_2),.rready(rready_2),.awaddr(awaddr_2),.awvalid(awvalid_2),.awready(awready_2),.wdata(wdata_2),.wstrb(wstrb_2),.wvalid(wvalid_2),.wready(wready_2),.bresp(bresp_2),.bvalid(bvalid_2),.bready(bready_2));
-mem2 #(64,64)mem_2(
-.clk(clk),
-.rst(rst),
-.r_ren(data_ram_en),
-.r_raddr(ram_addr),
-.r_rdata(ram_data),
-.r_waddr(ram_addr),
-.r_mask(wmask),
-.r_wen(data_ram_wen),
-.r_wdata(src2),
-.l_choose(l_choose),
-.inst_update(inst_update),
-.mem_finish(mem_finish),
-.inst_finish(inst_finish),
-.araddr2(araddr2),
-.arvalid2(arvalid2),
-.arburst2(arburst2),
-.arlen2(arlen2),
-.arsize2(arsize2),
-.arready2(arready2),
-.rdata2(rdata2),
-.rresp2(rresp2),
-.rvalid2(rvalid2),
-.rlast2(rlast2),
-.rready2(rready2),
-.awaddr2(awaddr2),
-.awvalid2(awvalid2),
-.awburst2(awburst2),
-.awlen2(awlen2),
-.awready2(awready2),
-.wdata2(wdata2),
-.wlast2(wlast2),
-.wstrb2(wstrb2),
-.wvalid2(wvalid2),
-.wready2(wready2),
-.bresp2(bresp2),
-.bvalid2(bvalid2),
-.bready2(bready2)
-);
-
+	mem2 #(64,64)mem_2(
+		.clk(clk),
+		.rst(rst),
+		.r_ren(data_ram_en),
+		.r_raddr(ram_addr),
+		.r_rdata(ram_data),
+		.r_waddr(ram_addr),
+		.r_mask(wmask),
+		.r_wen(data_ram_wen),
+		.r_wdata(src2),
+		.l_choose(l_choose),
+		.inst_update(inst_update),
+		.mem_finish(mem_finish),
+		.inst_finish(inst_finish),
+		.araddr2(araddr2),
+		.arvalid2(arvalid2),
+		.arburst2(arburst2),
+		.arlen2(arlen2),
+		.arsize2(arsize2),
+		.arready2(arready2),
+		.rdata2(rdata2),
+		.rresp2(rresp2),
+		.rvalid2(rvalid2),
+		.rlast2(rlast2),
+		.rready2(rready2),
+		.awaddr2(awaddr2),
+		.awvalid2(awvalid2),
+		.awburst2(awburst2),
+		.awlen2(awlen2),
+		.awready2(awready2),
+		.wdata2(wdata2),
+		.wlast2(wlast2),
+		.wstrb2(wstrb2),
+		.wvalid2(wvalid2),
+		.wready2(wready2),
+		.bresp2(bresp2),
+		.bvalid2(bvalid2),
+		.bready2(bready2)
+	);
+/////////////////////////////////////////
+///////////////////wb////////////////////
 	//写回模块
-wb wb0(.r_data(ram_data),.alu_result(alu_result),.sel_rf_res(sel_rf_res),.wdata(wdata),.c_rdata(c_rdata));
+	wb wb0(.r_data(ram_data),.alu_result(alu_result),.sel_rf_res(sel_rf_res),.wdata(wdata),.c_rdata(c_rdata));
+////////////////////////////////////////
 	//总线仲裁器
-arbiter arbiter_1(
-.clk(clk),
-.rst(rst),
-.araddr_1(araddr1),
-.araddr_2(araddr2),
-.arvalid_1(arvalid1),
-.arvalid_2(arvalid2),
-.arburst_1(arburst1),
-.arburst_2(arburst2),
-.arlen_1(arlen1),
-.arlen_2(arlen2),
-.arsize_1(arsize1),
-.arsize_2(arsize2),
-.arready_1(arready1),
-.arready_2(arready2),
-.rdata_1(rdata1),
-.rdata_2(rdata2),
-.rresp_1(rresp1),
-.rresp_2(rresp2),
-.rvalid_1(rvalid1),
-.rvalid_2(rvalid2),
-.rlast_1(rlast1),
-.rlast_2(rlast2),
-.rready_1(rready1),
-.rready_2(rready2),
-.awaddr_2(awaddr2),
-.awvalid_2(awvalid2),
-.awburst_2(awburst2),
-.awlen_2(awlen2),
-.awready_2(awready2),
-.wdata_2(wdata2),
-.wlast_2(wlast2),
-.wstrb_2(wstrb2),
-.wvalid_2(wvalid2),
-.wready_2(wready2),
-.bresp_2(bresp2),
-.bvalid_2(bvalid2),
-.bready_2(bready2),
-.inst_update(inst_update),
-.mem_finish(mem_finish)
-);
+	arbiter arbiter_1(
+		.clk(clk),
+		.rst(rst),
+		.araddr_1(araddr1),
+		.araddr_2(araddr2),
+		.arvalid_1(arvalid1),
+		.arvalid_2(arvalid2),
+		.arburst_1(arburst1),
+		.arburst_2(arburst2),
+		.arlen_1(arlen1),
+		.arlen_2(arlen2),
+		.arsize_1(arsize1),
+		.arsize_2(arsize2),
+		.arready_1(arready1),
+		.arready_2(arready2),
+		.rdata_1(rdata1),
+		.rdata_2(rdata2),
+		.rresp_1(rresp1),
+		.rresp_2(rresp2),
+		.rvalid_1(rvalid1),
+		.rvalid_2(rvalid2),
+		.rlast_1(rlast1),
+		.rlast_2(rlast2),
+		.rready_1(rready1),
+		.rready_2(rready2),
+		.awaddr_2(awaddr2),
+		.awvalid_2(awvalid2),
+		.awburst_2(awburst2),
+		.awlen_2(awlen2),
+		.awready_2(awready2),
+		.wdata_2(wdata2),
+		.wlast_2(wlast2),
+		.wstrb_2(wstrb2),
+		.wvalid_2(wvalid2),
+		.wready_2(wready2),
+		.bresp_2(bresp2),
+		.bvalid_2(bvalid2),
+		.bready_2(bready2),
+		.inst_update(inst_update),
+		.mem_finish(mem_finish)
+	);
+///////////////////////////////////////////
 
  	/////////////////////////////开启波形图/////////////////////
 	/*initial begin*/
