7f262abe5eed8410590b89c799a90cc1a60443eb
ysyx_22050533-武泽伟 <17805481057@163.com>
2023-05-31 11:00:18 +0800

    git test4

---
+++ nanos-lite/build/riscv64-nemu/src/proc.d
@@ -0,0 +1,17 @@
+/home/wzw/ysyx-workbench/nanos-lite/build/riscv64-nemu/./src/proc.o: \
+ /home/wzw/ysyx-workbench/nanos-lite/src/proc.c \
+ /home/wzw/ysyx-workbench/nanos-lite/include/proc.h \
+ /home/wzw/ysyx-workbench/nanos-lite/include/common.h \
+ /home/wzw/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/wzw/ysyx-workbench/navy-apps/libs/libc/include/stdint.h \
+ /home/wzw/ysyx-workbench/navy-apps/libs/libc/include/machine/_default_types.h \
+ /home/wzw/ysyx-workbench/navy-apps/libs/libc/include/sys/features.h \
+ /home/wzw/ysyx-workbench/navy-apps/libs/libc/include/_newlib_version.h \
+ /home/wzw/ysyx-workbench/navy-apps/libs/libc/include/sys/_intsup.h \
+ /home/wzw/ysyx-workbench/navy-apps/libs/libc/include/sys/_stdint.h \
+ /home/wzw/ysyx-workbench/abstract-machine/am/include/arch/riscv64-nemu.h \
+ /home/wzw/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/wzw/ysyx-workbench/abstract-machine/klib/include/klib.h \
+ /home/wzw/ysyx-workbench/abstract-machine/klib/include/klib-macros.h \
+ /home/wzw/ysyx-workbench/nanos-lite/include/debug.h \
+ /home/wzw/ysyx-workbench/nanos-lite/include/memory.h
---
+++ nanos-lite/build/riscv64-nemu/src/resources.d
@@ -0,0 +1,2 @@
+/home/wzw/ysyx-workbench/nanos-lite/build/riscv64-nemu/./src/resources.o: \
+ /home/wzw/ysyx-workbench/nanos-lite/src/resources.S
